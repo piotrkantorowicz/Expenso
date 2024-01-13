ad6e171357d431cd9236d144f3e811df4a8d0dd5
Piotr Kantorowicz <pkantorowicz@proton.me>
2024-01-13 11:21:20 +0100

    Modified migrations script

--- Scripts/add-migrations.ps1
+++ Scripts/add-migrations.ps1
@@ -19,7 +19,10 @@ The directory of the project where the migration will be added.
 The directory where the migration files will be outputted.
 
 .PARAMETER startUpProjectDir
-The directory of the startup project (optional).
+The directory of the startup project.
+
+.PARAMETER Help
+Displays the help documentation for the script
 
 .EXAMPLE
 .\run-migrations.ps1 -migrationName "InitialMigration" -dbContext "MyDbContext" -projectDir "MyProject" -outputDir "Migrations"
