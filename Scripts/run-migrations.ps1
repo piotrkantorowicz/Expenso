bc132bc024e76840065884bd19cfc7214a3e9708
Piotr Kantorowicz <pkantorowicz@proton.me>
2024-01-13 11:19:19 +0100

    Created generic db context factory

---
+++ Scripts/run-migrations.ps1
@@ -0,0 +1,52 @@
+<#
+.SYNOPSIS
+A script to run migrations for a .NET project.
+
+.DESCRIPTION
+This script runs a migration for a .NET project using the Entity Framework Core CLI. 
+It takes in parameters for the migration name, DbContext, project directory, output directory, and optionally the startup project directory.
+
+.PARAMETER migrationName
+The name of the migration to be added.
+
+.PARAMETER dbContext
+The name of the DbContext to be used.
+
+.PARAMETER projectDir
+The directory of the project where the migration will be added.
+
+.PARAMETER outputDir
+The directory where the migration files will be outputted.
+
+.PARAMETER startUpProjectDir
+The directory of the startup project (optional).
+
+.EXAMPLE
+.\run-migrations.ps1 -migrationName "InitialMigration" -dbContext "MyDbContext" -projectDir "MyProject" -outputDir "Migrations"
+#>
+
+param (
+    [string]$migrationName,
+    [string]$dbContext,  
+    [string]$projectDir,    
+    [string]$outputDir,
+    [string]$startUpProjectDir,
+    [switch]$Help
+)
+
+if ($Help) {
+    Get-Help $MyInvocation.MyCommand.Definition -Full
+    exit
+}
+
+if (!$migrationName -or !$dbContext -or !$projectDir -or !$outputDir -or !$startUpProjectDir) {
+    throw "The parameters -migrationName, -dbContext, -projectDir, -outputDir and -startUpProjectDir are required."
+}
+
+$initialLocation = Get-Location
+
+Set-Location $projectDir
+
+dotnet ef migrations add $migrationName --output-dir $outputDir --context $dbContext -- $startUpProjectDir
+
+Set-Location $initialLocation
---
+++ Src/Shared/Expenso.Shared.Database.EfCore.NpSql/DbContexts/NpsqlDbContextFactory.cs
@@ -0,0 +1,46 @@
+using System.Text;
+
+using Expenso.Shared.Configuration.Extensions;
+using Expenso.Shared.Configuration.Sections;
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Expenso.Shared.Database.EfCore.NpSql.DbContexts;
+
+public abstract class NpsqlDbContextFactory<TDbContext> : IDesignTimeDbContextFactory<TDbContext>
+    where TDbContext : DbContext
+{
+    private const string SettingsFileName = "appsettings";
+
+    public TDbContext CreateDbContext(string[]? args)
+    {
+        DbContextOptionsBuilder<TDbContext> optionsBuilder = new();
+        string? environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        string? startupProjectPath = args?[0];
+
+        if (string.IsNullOrEmpty(startupProjectPath) || !Path.Exists(startupProjectPath))
+        {
+            string errorMessage = new StringBuilder()
+                .Append(
+                    "Startup project path parameter must be provided and must exists on current machine. Actual value: ")
+                .Append(startupProjectPath)
+                .ToString();
+
+            throw new ArgumentException(nameof(startupProjectPath), errorMessage);
+        }
+
+        IConfiguration configuration = new ConfigurationBuilder()
+            .SetBasePath(startupProjectPath)
+            .AddJsonFile($"{SettingsFileName}.json", false, true)
+            .AddJsonFile($"{SettingsFileName}.{environment}.json", true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        configuration.TryBindOptions(SectionNames.EfCoreSection, out EfCoreSettings databaseSettings);
+        optionsBuilder.UseNpgsql(databaseSettings.ConnectionString);
+
+        return (TDbContext)Activator.CreateInstance(typeof(TDbContext), [optionsBuilder.Options])!;
+    }
+}
