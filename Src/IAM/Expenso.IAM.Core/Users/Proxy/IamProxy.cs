9df3b610b50264b9c4f9c2f7969fa7dcacf3eb94
Piotr Kantorowicz <pkantorowicz@proton.me>
2024-01-28 07:11:27 +0100

    Added cancellation tokens to proxy methods

--- Src/IAM/Expenso.IAM.Core/Users/Proxy/IamProxy.cs
+++ Src/IAM/Expenso.IAM.Core/Users/Proxy/IamProxy.cs
@@ -10,13 +10,15 @@ internal sealed class IamProxy(IQueryDispatcher queryDispatcher) : IIamProxy
     private readonly IQueryDispatcher _queryDispatcher =
         queryDispatcher ?? throw new ArgumentNullException(nameof(queryDispatcher));
 
-    public async Task<GetUserInternalResponse?> GetUserByIdAsync(string userId)
+    public async Task<GetUserInternalResponse?> GetUserByIdAsync(string userId,
+        CancellationToken cancellationToken = default)
     {
-        return await _queryDispatcher.QueryAsync(new GetUserQuery(userId));
+        return await _queryDispatcher.QueryAsync(new GetUserQuery(userId), cancellationToken);
     }
 
-    public async Task<GetUserInternalResponse?> GetUserByEmailAsync(string email)
+    public async Task<GetUserInternalResponse?> GetUserByEmailAsync(string email,
+        CancellationToken cancellationToken = default)
     {
-        return await _queryDispatcher.QueryAsync(new GetUserQuery(Email: email));
+        return await _queryDispatcher.QueryAsync(new GetUserQuery(Email: email), cancellationToken);
     }
 }
