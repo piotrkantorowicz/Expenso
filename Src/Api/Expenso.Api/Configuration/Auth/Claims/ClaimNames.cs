58c04bbb7a2791e36a4a1b47012192f42628969d
Piotr Kantorowicz <pkantorowicz@proton.me>
2024-01-28 07:05:39 +0100

    Fixed problems with getting user from user context

--- Src/Api/Expenso.Api/Configuration/Auth/Claims/ClaimNames.cs
+++ Src/Api/Expenso.Api/Configuration/Auth/Claims/ClaimNames.cs
@@ -1,7 +1,9 @@
+using System.Security.Claims;
+
 namespace Expenso.Api.Configuration.Auth.Claims;
 
 internal static class ClaimNames
 {
-    public const string UserIdClaimName = "user_id";
+    public const string UserIdClaimName = ClaimTypes.NameIdentifier;
     public const string UsernameClaimName = "name";
 }
