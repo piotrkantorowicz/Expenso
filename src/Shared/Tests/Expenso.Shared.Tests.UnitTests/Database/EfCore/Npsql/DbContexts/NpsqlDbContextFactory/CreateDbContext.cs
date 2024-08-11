1f30562eeb543828e7e7863544b1bce518022f71
Piotr Kantorowicz <pkantorowicz@proton.me>
2024-08-11 06:49:11 +0200

    Change naming of some directories

--- src/Shared/Tests/Expenso.Shared.Tests.UnitTests/Database/EfCore/Npsql/DbContexts/NpsqlDbContextFactory/CreateDbContext.cs
+++ src/Shared/Tests/Expenso.Shared.Tests.UnitTests/Database/EfCore/Npsql/DbContexts/NpsqlDbContextFactory/CreateDbContext.cs
@@ -30,7 +30,7 @@ internal sealed class CreateDbContext : NpsqlDbContextFactoryTestBase
             "..",
             "..",
             "..",
-            "Src",
+            "src",
             "Api",
             "Expenso.Api"
         ]);
