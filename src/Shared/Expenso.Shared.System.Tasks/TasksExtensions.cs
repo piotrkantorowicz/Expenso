55b7ff8d98648e81db2939d0ed6d90724f3c850c
Piotr Kantorowicz <87525551+piotrkantorowicz@users.noreply.github.com>
2024-08-12 06:21:23 +0200

    Create project for tasks extensions (#67)

---
+++ src/Shared/Expenso.Shared.System.Tasks/TasksExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace Expenso.Shared.System.Tasks;
+
+public static class TaskExtensions
+{
+    public static T RunAsSync<T>(this Task<T> task)
+    {
+        return task.ConfigureAwait(continueOnCapturedContext: false).GetAwaiter().GetResult();
+    }
+
+    public static void RunAsSync(this Task task)
+    {
+        task.ConfigureAwait(continueOnCapturedContext: false).GetAwaiter().GetResult();
+    }
+}
