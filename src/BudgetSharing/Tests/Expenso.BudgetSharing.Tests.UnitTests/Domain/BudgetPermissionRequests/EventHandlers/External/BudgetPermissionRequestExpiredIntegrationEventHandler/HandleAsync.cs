0123bb473a8863336f5f23197bf3b21543a21255
Piotr Kantorowicz <pkantorowicz@proton.me>
2024-08-13 05:57:36 +0200

    Add basic test for new handler

---
+++ src/BudgetSharing/Tests/Expenso.BudgetSharing.Tests.UnitTests/Domain/BudgetPermissionRequests/EventHandlers/External/BudgetPermissionRequestExpiredIntegrationEventHandler/HandleAsync.cs
@@ -0,0 +1,20 @@
+﻿using Expenso.BudgetSharing.Proxy.DTO.MessageBus.BudgetPermissionRequests;
+
+namespace Expenso.BudgetSharing.Tests.UnitTests.Domain.BudgetPermissionRequests.EventHandlers.External.
+    BudgetPermissionRequestExpiredIntegrationEventHandler;
+
+internal sealed class HandleAsync : BudgetPermissionRequestExpiredIntegrationEventHandlerTestBase
+{
+    [Test]
+    public void Should_NotThrow()
+    {
+        // Arrange
+        // Act
+        // Assert
+        Assert.DoesNotThrowAsync(code: () =>
+            TestCandidate.HandleAsync(
+                @event: new BudgetPermissionRequestExpiredIntegrationEvent(
+                    MessageContext: MessageContextFactoryMock.Object.Current(),
+                    BudgetPermissionRequestId: Guid.NewGuid()), cancellationToken: default));
+    }
+}
