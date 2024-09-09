d328d58eabaeeb6c4d407205688ad4090f1c9f66
Piotr Kantorowicz <pkantorowicz@proton.me>
2024-09-09 05:57:33 +0200

    Apply code review suggestions

--- docker/sql/init.sql
+++ docker/sql/init.sql
@@ -1,11 +1,22 @@
-CREATE DATABASE keycloak;
-CREATE USER keycloak WITH ENCRYPTED PASSWORD 'Keycloak321!';
-GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
-\c keycloak postgres
-GRANT ALL ON SCHEMA public TO keycloak;
+BEGIN;
 
-CREATE DATABASE expenso;
-CREATE USER expenso WITH ENCRYPTED PASSWORD 'Expenso321!';
-GRANT ALL PRIVILEGES ON DATABASE expenso TO expenso;
-\c expenso postgres
-GRANT ALL ON SCHEMA public TO expenso;
+DO
+$$
+    BEGIN
+        CREATE DATABASE keycloak;
+        CREATE USER keycloak WITH ENCRYPTED PASSWORD '<your_password>';
+        GRANT ALL PRIVILEGES ON DATABASE keycloak TO keycloak;
+        EXECUTE 'GRANT ALL ON SCHEMA public TO keycloak' USING 'keycloak';
+
+        CREATE DATABASE expenso;
+        CREATE USER expenso WITH ENCRYPTED PASSWORD '<your_password>';
+        GRANT ALL PRIVILEGES ON DATABASE expenso TO expenso;
+        EXECUTE 'GRANT ALL ON SCHEMA public TO expenso' USING 'expenso';
+
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN
+            ROLLBACK;
+            RAISE;
+    END
+$$;
