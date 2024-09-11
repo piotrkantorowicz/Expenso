507bbfe78729597cefc3c0f5a591506e3e060362
Piotr Kantorowicz <pkantorowicz@proton.me>
2024-09-11 04:13:12 +0200

    Add tests for notification settings extensions

---
+++ src/Communication/Expenso.Communication.Proxy/DTO/API/SendNotification/Extensions/CreateNotificationRequestNotificationTypeExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Expenso.Communication.Proxy.DTO.Settings;
+
+namespace Expenso.Communication.Proxy.DTO.API.SendNotification.Extensions;
+
+public static class CreateNotificationRequestNotificationTypeExtensions
+{
+    public static SendNotificationRequest_NotificationType CreateNotificationTypeBasedOnSettings(
+        this NotificationSettings? settings)
+    {
+        if (settings is null)
+        {
+            return Disable();
+        }
+
+        if (settings.Enabled is false)
+        {
+            return Disable();
+        }
+
+        return new SendNotificationRequest_NotificationType(Email: settings.Email.Enabled,
+            InApp: settings.InApp.Enabled, Push: settings.Push.Enabled);
+    }
+
+    private static SendNotificationRequest_NotificationType Disable()
+    {
+        return new SendNotificationRequest_NotificationType(Email: false, Push: false, InApp: false);
+    }
+}
--- src/Communication/Expenso.Communication.Proxy/DTO/API/SendNotification/SendNotificationRequest_NotificationType.cs
+++ src/Communication/Expenso.Communication.Proxy/DTO/API/SendNotification/SendNotificationRequest_NotificationType.cs
@@ -1,27 +1,6 @@
-﻿using Expenso.Communication.Proxy.DTO.Settings;
+﻿namespace Expenso.Communication.Proxy.DTO.API.SendNotification;
 
-namespace Expenso.Communication.Proxy.DTO.API.SendNotification;
-
-public sealed record SendNotificationRequest_NotificationType(bool? Email = null, bool? Push = null, bool? InApp = null)
-{
-    public static SendNotificationRequest_NotificationType FromSettings(NotificationSettings? settings)
-    {
-        if (settings is null)
-        {
-            return Disable();
-        }
-
-        if (settings.Enabled is false)
-        {
-            return Disable();
-        }
-
-        return new SendNotificationRequest_NotificationType(Email: settings.Email.Enabled,
-            InApp: settings.InApp.Enabled, Push: settings.Push.Enabled);
-    }
-
-    public static SendNotificationRequest_NotificationType Disable()
-    {
-        return new SendNotificationRequest_NotificationType(Email: false, Push: false, InApp: false);
-    }
-}
+public sealed record SendNotificationRequest_NotificationType(
+    bool? Email = null,
+    bool? Push = null,
+    bool? InApp = null);
