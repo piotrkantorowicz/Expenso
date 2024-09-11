321b6429fee046163c0e9bb495df16bc35fa6c9d
Piotr Kantorowicz <pkantorowicz@proton.me>
2024-09-11 05:19:39 +0200

    Add unit test to module id middleware

--- src/Api/Expenso.Api/Configuration/Execution/Middlewares/ModuleIdMiddleware.cs
+++ src/Api/Expenso.Api/Configuration/Execution/Middlewares/ModuleIdMiddleware.cs
@@ -1,12 +1,11 @@
-﻿using Expenso.Shared.System.Logging;
-using Expenso.Shared.System.Modules;
+﻿using Expenso.Api.Configuration.Extensions;
+using Expenso.Shared.System.Logging;
 
 namespace Expenso.Api.Configuration.Execution.Middlewares;
 
 internal sealed class ModuleIdMiddleware
 {
     internal const string ModuleMiddlewareHeaderKey = "Module";
-    private readonly IReadOnlyCollection<string> _managementPaths = ["health", "metrics", "swagger"];
     private readonly RequestDelegate _next;
 
     public ModuleIdMiddleware(RequestDelegate next)
@@ -19,7 +18,8 @@ internal sealed class ModuleIdMiddleware
         ILoggerService<ModuleIdMiddleware>? logger =
             context.RequestServices.GetService<ILoggerService<ModuleIdMiddleware>>();
 
-        string? moduleId = GuessModule(logger: logger, requestPath: context.Request.Path);
+        string? requestPath = context.Request.Path;
+        string? moduleId = requestPath.GuessModule(logger: logger);
         context.Request.Headers.Append(key: ModuleMiddlewareHeaderKey, value: moduleId);
 
         context.Response.OnStarting(callback: () =>
@@ -30,48 +30,5 @@ internal sealed class ModuleIdMiddleware
         });
 
         await _next.Invoke(context: context);
-    }
-
-    private string? GuessModule(ILoggerService<ModuleIdMiddleware>? logger, string? requestPath)
-    {
-        if (requestPath is not null && _managementPaths.Any(predicate: requestPath.Contains))
-        {
-            logger?.LogDebug(eventId: LoggingUtils.GeneralInformation,
-                message: "Management urls cannot be used to define module names");
-
-            return null;
-        }
-
-        IDictionary<string, ModuleDefinition> registeredModules = Modules.GetRegisteredModules();
-
-        if (!registeredModules.Any())
-        {
-            logger?.LogWarning(eventId: LoggingUtils.GeneralWarning, message: "No registered modules found");
-
-            return null;
-        }
-
-        var modulePrefixes = Modules
-            .GetRegisteredModules()
-            .Select(selector: x => new
-            {
-                Prefix = x.Value.ModulePrefix,
-                Name = x.Value.ModuleName
-            })
-            .ToList();
-
-        foreach (var modulePrefix in modulePrefixes.Where(predicate: modulePrefix =>
-                     requestPath?.Contains(value: modulePrefix.Prefix) == true))
-        {
-            logger?.LogDebug(eventId: LoggingUtils.GeneralInformation,
-                message: $"Module found: {modulePrefix.Name} for request path: {requestPath}");
-
-            return modulePrefix.Name;
-        }
-
-        logger?.LogWarning(eventId: LoggingUtils.GeneralWarning,
-            message: $"No matching module found for request path: {requestPath}");
-
-        return null;
     }
 }
---
+++ src/Api/Expenso.Api/Configuration/Extensions/RequestPathExtensions.cs
@@ -0,0 +1,60 @@
+﻿using Expenso.Api.Configuration.Execution.Middlewares;
+using Expenso.Shared.System.Logging;
+using Expenso.Shared.System.Modules;
+
+namespace Expenso.Api.Configuration.Extensions;
+
+internal static class RequestPathExtensions
+{
+    private static readonly IReadOnlyCollection<string> ManagementPaths = ["health", "metrics", "swagger"];
+
+    internal static string? GuessModule(this string? requestPath, ILoggerService<ModuleIdMiddleware>? logger)
+    {
+        if (requestPath is null)
+        {
+            logger?.LogWarning(eventId: LoggingUtils.GeneralInformation, message: "Request path is null or empty");
+
+            return null;
+        }
+
+        if (ManagementPaths.Any(predicate: requestPath.Contains))
+        {
+            logger?.LogDebug(eventId: LoggingUtils.GeneralInformation,
+                message: "Management urls cannot be used to define module names");
+
+            return null;
+        }
+
+        IDictionary<string, ModuleDefinition> registeredModules = Modules.GetRegisteredModules();
+
+        if (!registeredModules.Any())
+        {
+            logger?.LogWarning(eventId: LoggingUtils.GeneralWarning, message: "No registered modules found");
+
+            return null;
+        }
+
+        var modulePrefixes = Modules
+            .GetRegisteredModules()
+            .Select(selector: x => new
+            {
+                Prefix = x.Value.ModulePrefix,
+                Name = x.Value.ModuleName
+            })
+            .ToList();
+
+        foreach (var modulePrefix in modulePrefixes.Where(predicate: modulePrefix =>
+                     requestPath?.Contains(value: modulePrefix.Prefix) == true))
+        {
+            logger?.LogDebug(eventId: LoggingUtils.GeneralInformation,
+                message: $"Module found: {modulePrefix.Name} for request path: {requestPath}");
+
+            return modulePrefix.Name;
+        }
+
+        logger?.LogWarning(eventId: LoggingUtils.GeneralWarning,
+            message: $"No matching module found for request path: {requestPath}");
+
+        return null;
+    }
+}
