c84bb0667ed5940eaa4d5a0216968679e570be66
Piotr Kantorowicz <pkantorowicz@proton.me>
2024-10-30 05:31:43 +0100

    Add tests for budget sharing proxy

---
+++ src/BudgetSharing/Tests/Expenso.BudgetSharing.Tests.UnitTests/Application/Proxy/BudgetSharingProxy/GetBudgetPermissionsAsync.cs
@@ -0,0 +1,41 @@
+﻿using Expenso.BudgetSharing.Application.BudgetPermissions.Read.GetBudgetPermissions;
+using Expenso.BudgetSharing.Shared.DTO.API.BudgetPermissions.GetBudgetPermissions.Request;
+using Expenso.BudgetSharing.Shared.DTO.API.BudgetPermissions.GetBudgetPermissions.Response;
+
+using FluentAssertions;
+
+using Moq;
+
+namespace Expenso.BudgetSharing.Tests.UnitTests.Application.Proxy.BudgetSharingProxy;
+
+[TestFixture]
+internal sealed class GetBudgetPermissionsAsync : BudgetSharingProxyTestBase
+{
+    [Test]
+    public async Task Should_ReturnBudgetPermissions_When_RequestIsValid()
+    {
+        // Arrange
+        GetBudgetPermissionsRequest request = new();
+
+        List<GetBudgetPermissionsResponse> response =
+        [
+            new(Id: Guid.NewGuid(), BudgetId: Guid.NewGuid(), OwnerId: Guid.NewGuid(),
+                Permissions: new List<GetBudgetPermissionsResponse_Permission>
+                {
+                    new(ParticipantId: Guid.NewGuid(),
+                        PermissionType: GetBudgetPermissionsResponse_PermissionType.Reviewer)
+                })
+        ];
+
+        _queryDispatcherMock
+            .Setup(expression: q => q.QueryAsync(It.IsAny<GetBudgetPermissionsQuery>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(value: response);
+
+        // Act
+        IReadOnlyCollection<GetBudgetPermissionsResponse>? result =
+            await TestCandidate.GetBudgetPermissionsAsync(request: request);
+
+        // Assert
+        result.Should().BeEquivalentTo(expectation: response);
+    }
+}
